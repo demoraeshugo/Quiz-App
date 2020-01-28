31fa76d52181c1abd3b92247b40f26a6365dce4c
Hugo De Moraes <demoraeshugo@gmail.com>
2020-01-27 20:08:53 -0500

    Initial Json data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import questions from "../src/questions.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Testing AWS services :)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component  {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: questions
+    }; 
+  } 
+
+  render() {
+
+    return (
+      //<Header/>
+      //<Main/>
+      //Footer/>
+    )
+  }
 }
 
 export default App;
