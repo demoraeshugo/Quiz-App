929837f318e5e6a0d10ec018421c2ae248298c27
Hugo De Moraes <demoraeshugo@gmail.com>
2020-01-30 14:00:54 -0500

    Text Changes

--- src/Components/Main/About-Page/index.js
+++ src/Components/Main/About-Page/index.js
@@ -26,13 +26,12 @@ const AboutPage = () => {
         </p>
         <h4>Why did you build your site the way you did?</h4>
         <p>
-          I wanted to create a "native" like experience on a web platform. To
-          accomplish this, I took a mobile-first development approach to ensure
-          users across different platforms would have a satisfactory experience
-          The questions are actually fully modular. Questions and answers are
-          fed to the app via JSON file. For this project I used 5 questions each
-          with 3 asnwers, but this can very easily be expanded by modifying the
-          JSON instead of the whole app.
+          I wanted to create a "native" like experience on a web platform. I
+          took a mobile-first development approach to ensure users across
+          different platforms would have a satisfactory experience. Questions
+          and answers are fed to the app via JSON file. For this project I used
+          5 questions each with 3 answers, but this can very easily be expanded
+          by modifying the JSON file.
         </p>
         <h4>What frameworks, if any, did you use, and why?</h4>
         <p>
@@ -40,14 +39,12 @@ const AboutPage = () => {
           consisted of the application's logic and design. Prior to starting any
           coding I spent time scheming the app's architecture and used Adobe XD
           to wireframe a design. I enjoy creating React apps becuase they allow
-          developers to create faster and more intuitive user experiences.
-          React's virtual DOM means that only parts of the DOM that change need
-          to be rerendered, which makes for faster updating and rendering. As
-          mentioned above, I wireframed this project using Adobe XD. I then used
-          Sass as a CSS preprocesser becuase of it's customizability and ease.
-          All the code is hosted on Github. A live version is being hosted via
-          AWS conected to my repository. This was a very nice setup that allowed
-          me to develop and deploy within minutes.
+          for faster and more intuitive user experiences. mentioned above, I
+          wireframed this project using Adobe XD. I then used Sass as a CSS
+          preprocesser becuase of it's customizability and ease. All the code is
+          hosted on Github. A live version is being hosted via AWS conected to
+          my repository. This was a very nice setup that allowed me to develop
+          and deploy within minutes.
         </p>
       </FadeIn>
     </div>
